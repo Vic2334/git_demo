1eddeeddef08274f666abaee6690120ec4484356
llg <3189609554@qq.com>
2023-06-28 21:00:16 +0800

    feature 提交

--- src/main/java/cn/llg/Main.java
+++ src/main/java/cn/llg/Main.java
@@ -8,5 +8,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello git!");
         System.out.println("修改代码");
+        System.out.println("feature");
     }
 }
