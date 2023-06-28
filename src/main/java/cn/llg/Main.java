b257e57000db38c7f46dc56ad4979aff3798359a
llg <3189609554@qq.com>
2023-06-28 20:32:37 +0800

    第二次提交git_demo

--- src/main/java/cn/llg/Main.java
+++ src/main/java/cn/llg/Main.java
@@ -6,6 +6,7 @@ package cn.llg;
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello git!");
+        System.out.println("修改代码");
     }
 }
